c37d874043b985846acabc823e6a9c360c2b52d3
fernandoapeguero <fernandoapeguero@gmail.com>
2022-06-01 17:40:35 -0400

    Add comment endpoint and functionality

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import bodyParser from "body-parser";
 const articlesInfo = {
   "learn-react": {
     upvotes: 0,
+    comments: [],
   },
   "learn-node": {
     upvotes: 0,
+    comments: [],
   },
   "my-thoughts-on-resume": {
     upvotes: 0,
+    comments: [],
   },
 };
 
@@ -27,6 +30,18 @@ app.post("/api/articles/:name/upvote", (req, res) => {
     .send(
       `${articleName} now has ${articlesInfo[articleName].upvotes} number of upvotes.`
     );
+});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({
+    username,
+    text,
+  });
+
+  res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(PORT, () => {
